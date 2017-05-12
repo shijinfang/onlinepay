949aaa2a1affdc59db9aa3c5b8f8e413e9a0f8b9
shijifang <jacky5154@outlook.com>
2017-05-13 06:54:29 +0800

    修改打包成war

--- src/main/java/com/physical/PhysicalPayApplication.java
+++ src/main/java/com/physical/PhysicalPayApplication.java
@@ -3,14 +3,20 @@ package com.physical;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class PhysicalPayApplication implements CommandLineRunner {
+public class PhysicalPayApplication  extends SpringBootServletInitializer implements CommandLineRunner  {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PhysicalPayApplication.class, args);
 	}
 
+	@Override
+	public SpringApplicationBuilder configure(SpringApplicationBuilder application){
+		return application.sources(PhysicalPayApplication.class);
+	}
 	@Override
 	public void run(String... arg0) throws Exception {
 		// TODO Auto-generated method stub
